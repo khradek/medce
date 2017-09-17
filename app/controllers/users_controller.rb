a74d9c96cad56f62e78ea4a5147185b192c76382
Kevin Hradek <khradek@gmail.com>
2017-09-17 16:56:50 -0400

    updated author page to show only published courses and added all course button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @courses = @user.courses.order("title").paginate(:page => params[:page], :per_page => 5)
+    @courses = @user.courses.where('published = ?', true ).order("title").paginate(:page => params[:page], :per_page => 5)
     @order_item = current_order.order_items.new
     if current_user
       @purchased_courses = current_user.purchased_courses
