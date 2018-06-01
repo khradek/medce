4caab0ae86178296d3dd84f483046ec0a94e5792
Kevin Hradek <khradek@gmail.com>
2018-05-31 20:05:48 -0400

    installed quotaguard

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,10 +2,10 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => S3_ACCESS,
-      :aws_secret_access_key  => S3_SECRET,
-      :region                 => 'us-east-2' # Change this for different AWS region. Default is 'us-east-1'
+    :provider               => 'AWS',
+    :aws_access_key_id      => S3_ACCESS,
+    :aws_secret_access_key  => S3_SECRET,
+    :region                 => 'us-east-2' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = 'medce'
 end
